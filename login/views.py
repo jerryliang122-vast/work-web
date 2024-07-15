4a3a27dfbe7ce8c0485c674d9eee11fe1550117e
jerryliang122 <1261449269@qq.com>
2024-07-15 17:27:54 +0800

    新增login/views.py的文档字符串
    为login/views.py的index函数添加了详细的文档字符串，说明了函数的功能、参数和返回值。这将有助于其他开发者更好地理解和使用这个函数。

--- login/views.py
+++ login/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def index(request):
+    """
+    处理主页请求并返回主页模板。
+
+    此函数接收一个HTTP请求，将其渲染成主页的HTML响应。
+    它是网站的入口点，用于展示主页的内容。
+
+    参数:
+    - request: HttpRequest对象，包含客户端的请求信息。
+
+    返回:
+    - HttpResponse对象，包含渲染后的主页HTML内容。
+    """
     return render(request, 'index.html')
